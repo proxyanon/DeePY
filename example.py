ee1d2680a1969f4e611354f2e0dfa19759f011bb
ProXySec <ondproxy@gmail.com>
2018-08-11 17:21:20 -0300

    Exemplo de uso da lib do DeePY

--- example.py
+++ example.py
@@ -1,70 +1,24 @@
-from pydeep import *
+from DeePY import Layer, Network
+import numpy as np
 
-# idade, renda, sexo, estadocivil, filhos
-# masculino = 1, feminino = 0
-# casado = 0, solteiro 1
+inputs = np.array([[1, 0, 0], [0, 1, 0], [1, 1, 0]])
+outputs = np.array([[1], [0], [1]])
 
-entradas = array([
-				 [18.0, 0.0, 1.0, 0.0, 0.0],
-				 [22.0, 1210.0, 1.0, 0.0, 0.0],
-				 [31.0, 2500.0, 0.0, 1.0, 1.0],
-				 [45.0, 1000.0, 0.0, 0.0, 0.0],
-				 [56.0, 1250.0, 1.0, 1.0, 3.0],
-				 [24.0, 2500.0, 0.0, 1.0, 0.0],
-				 [21.0, 980.0, 1.0, 0.0, 0.0],
-				 [20.0, 980.0, 0.0, 0.0, 0.0]
-				], dtype=float) / 10000
+l = Layer(True)
+l.create('input', sizes=[3, 3], activation='sigmoid')
+l.create('hidden', sizes=[3, 5], activation='relu')
+l.create('hidden', sizes=[5, 4], activation='sigmoid')
+l.create('output', sizes=[4, 1], activation='sigmoid')
+layers = l.layers()
 
-saidas = array([
-				 [0],
-				 [1],
-				 [1],
-				 [0],
-				 [1],
-				 [1],
-				 [0],
-				 [0]
-				])
+dpy = Network(layers=layers, eta=1, verbose=True)
+dpy.train(inputs=inputs, outputs=outputs, epochs=1500)
+dpy.saveweights()
+dpy.savemodel()
 
-entrada_teste = array([
-					 [40.0, 2000.0, 1.0, 1.0, 2.0],
-					 [33.0, 1578.0, 0.0, 0.0, 0.0],
-					 [26.0, 1000.0, 0.0, 0.0, 0.0],
-					 [30.0, 980.0, 1.0, 0.0, 1.0],
-					], dtype=float) / 10000
 
-saida_teste = array([
-					 [1.0],
-					 [1.0],
-					 [0.0],
-					 [0.0]
-					])
+ret = dpy.predict(inpt=np.array([0, 0, 0]))
+prediction = ret[len(ret)-1][0]['layer']
+rounded = round(prediction[0])
 
-entrada_desconhecida = array([
-						 [19.0, 550.0, 1.0, 0.0, 0.0], # 0
-						 [56.0, 2300.0, 0.0, 1.0, 3.0], # 1
-						 [33.0, 1800.0, 1.0, 0.0, 1.0], # 1
-						 [22.0, 2000.0, 1.0, 1.0, 0.0], # 1
-						 [20.0, 900.0, 0.0, 0.0, 0.0], # 0
-						 [48.0, 1100.0, 1.0, 0.0, 0.0], # 0
-						 [70.0, 2500.0, 0.0, 0.0, 1.0] # 1
-						]) / 10000
-
-nn = PyDeep(n_camadas=3, n_entradas=5, n_hidden=5, n_saida=1, eta=1, activation='sigmoid')
-
-for x, entrada in enumerate(entrada_teste):
-	gen_name = 'generation_{}/'.format(x)
-	nn.train(
-		 entradas=entradas, 
-		 saidas=saidas, 
-		 entrada=entrada, 
-		 saida=saida_teste[x], 
-		 create_generation=True, 
-		 gen_name=gen_name, 
-		 use_epochs=True, 
-		 epochs=150000,
-		 autodel=True
-		)
-
-for entrada in entrada_desconhecida:
-	print entrada * 10000, '=>', nn.run(entrada=entrada, path='generations/{}'.format(gen_name))
+print 'input =>', np.array([0, 0, 0]), 'predict =>', prediction, 'predict rounded =>', rounded
